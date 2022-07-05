72d6ec9b4d1f58e37f6058c623f2aa65c855101a
Alexandr93 <shurik.g93@gmail.com>
2022-07-05 12:13:23 +0300

    "New world1"

--- src/main/java/TestGit.java
+++ src/main/java/TestGit.java
@@ -2,5 +2,6 @@ public class TestGit {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("New world");
+        System.out.println("New world1");
     }
 }
